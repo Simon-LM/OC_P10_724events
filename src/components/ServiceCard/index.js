1c890ae9917d1c1f41184b11523bcb2f9ebae47c
Simon-LM <simon.lm.dev@pm.me>
2024-08-14 18:31:49 +0200

    Added anchor links with IDs for menu navigation

--- src/components/ServiceCard/index.js
+++ src/components/ServiceCard/index.js
@@ -1,24 +1,26 @@
+/** @format */
+
 import PropTypes from "prop-types";
 
 import "./style.scss";
 
 const ServiceCard = ({ imageSrc, imageAlt, children }) => (
-    <div className="ServiceCard">
-      <div className="ServiceCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-      </div>
-      <div className="ServiceCard__textContainer">{children}</div>
-    </div>
-  );
+	<div id="nos-services" className="ServiceCard">
+		<div className="ServiceCard__imageContainer">
+			<img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+		</div>
+		<div className="ServiceCard__textContainer">{children}</div>
+	</div>
+);
 
 ServiceCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
-  imageAlt: PropTypes.string,
-  children: PropTypes.node.isRequired,
+	imageSrc: PropTypes.string.isRequired,
+	imageAlt: PropTypes.string,
+	children: PropTypes.node.isRequired,
 };
 
 ServiceCard.defaultProps = {
-  imageAlt: "image"
-}
+	imageAlt: "image",
+};
 
 export default ServiceCard;
--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,44 +1,67 @@
+/** @format */
+
 import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./index";
 
 describe("When Form is created", () => {
-  it("a list of fields card is displayed", async () => {
-    render(<Home />);
-    await screen.findByText("Email");
-    await screen.findByText("Nom");
-    await screen.findByText("Prénom");
-    await screen.findByText("Personel / Entreprise");
-  });
+	it("a list of fields card is displayed", async () => {
+		render(<Home />);
+		await screen.findByText("Email");
+		await screen.findByText("Nom");
+		await screen.findByText("Prénom");
+		await screen.findByText("Personel / Entreprise");
+	});
 
-  describe("and a click is triggered on the submit button", () => {
-    it("the success message is displayed", async () => {
-      render(<Home />);
-      fireEvent(
-        await screen.findByText("Envoyer"),
-        new MouseEvent("click", {
-          cancelable: true,
-          bubbles: true,
-        })
-      );
-      await screen.findByText("En cours");
-      await screen.findByText("Message envoyé !");
-    });
-  });
-
+	describe("and a click is triggered on the submit button", () => {
+		it("the success message is displayed", async () => {
+			render(<Home />);
+			fireEvent(
+				await screen.findByText("Envoyer"),
+				new MouseEvent("click", {
+					cancelable: true,
+					bubbles: true,
+				})
+			);
+			await screen.findByText("En cours");
+			await screen.findByText("Message envoyé !");
+		});
+	});
 });
 
+// // // // // // // // // // // // // // //
+
+// import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+
+// test('When Form is created and a click is triggered on the submit button, the success message is displayed', async () => {
+//   render(<Form />);
+
+//   // Simulez le clic sur le bouton de soumission
+//   fireEvent.click(screen.getByRole('button', { name: /submit/i }));
+
+//   // Attendez que le texte "En cours" apparaisse
+//   await screen.findByText("En cours");
+
+//   // Attendez que le message de succès appar avec une requête plus flexible
+//   await waitFor(() => {
+//     expect(screen.getByText((content, element) => {
+//       return element.textContent.includes('Message envoyé');
+//     })).toBeInTheDocument();
+//   }, { timeout: 5000 }); // Augmentez le timeout si nécessaire
+// });
+
+// // // // // // // // // // // // //
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement
-  })
-  it("a list a people is displayed", () => {
-    // to implement
-  })
-  it("a footer is displayed", () => {
-    // to implement
-  })
-  it("an event card, with the last event, is displayed", () => {
-    // to implement
-  })
+	it("a list of events is displayed", () => {
+		// to implement
+	});
+	it("a list a people is displayed", () => {
+		// to implement
+	});
+	it("a footer is displayed", () => {
+		// to implement
+	});
+	it("an event card, with the last event, is displayed", () => {
+		// to implement
+	});
 });
