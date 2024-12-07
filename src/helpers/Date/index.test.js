d3356ee6ee48ecb7e4ce9473e637de09ae4afe55
Simon-LM <simon.lm.dev@bm.me>
2024-12-07 15:53:54 +0100

    Test for Date object, DONE

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,19 @@
-/**
- * 
- */
+/** @format */
+import { getMonth } from "./index";
 
 describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
-    });
-})
-
+	describe("When getMonth is called", () => {
+		it("the function return janvier for 2022-01-01 as date", () => {
+			// Create a new Date object for January
+			const date = new Date("2022-01-01");
+			// Test the function with this date
+			expect(getMonth(date)).toBe("janvier");
+		});
+		it("the function return juillet for 2022-07-08 as date", () => {
+			// Create a new Date object for July
+			const date = new Date("2022-07-08");
+			// Test the function with this date
+			expect(getMonth(date)).toBe("juillet");
+		});
+	});
+});
